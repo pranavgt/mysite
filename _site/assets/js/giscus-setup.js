cd4ee87567b97048ca889ab10ca6333ba0cbecb4
pranavgt <pranavguhathakurta@gmail.com>
2025-11-09 17:16:41 -0500

    11_09_2025_v8

--- _site/assets/js/giscus-setup.js
+++ _site/assets/js/giscus-setup.js
@@ -1,11 +1,16 @@
 function determineGiscusTheme() {
-  let theme = localStorage.getItem("theme") || document.documentElement.getAttribute("data-theme") || "system";
+  
+    let theme =
+      localStorage.getItem("theme") ||
+      document.documentElement.getAttribute("data-theme") ||
+      "system";
 
-  if (theme === "dark") return "dark";
-  if (theme === "light") return "light";
+    if (theme === "dark") return "dark";
+    if (theme === "light") return "light";
 
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  return prefersDark ? "dark" : "light";
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return prefersDark ? "dark" : "light";
+  
 }
 
 (function setupGiscus() {
@@ -29,6 +34,9 @@ function determineGiscusTheme() {
   };
 
   let giscusScript = document.createElement("script");
-  Object.entries(giscusAttributes).forEach(([key, value]) => giscusScript.setAttribute(key, value));
+  Object.entries(giscusAttributes).forEach(([key, value]) =>
+    giscusScript.setAttribute(key, value)
+  );
   document.getElementById("giscus_thread").appendChild(giscusScript);
 })();
+
